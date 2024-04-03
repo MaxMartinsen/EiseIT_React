b49e010ca2cb1584d6bde3b43ff0eeba42599fe5
Max Martinsen <max.martinsen@hotmail.com>
2024-04-03 20:45:10 +0200

    Update Aside.jsx

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -2,7 +2,7 @@ function Aside() {
   return (
     <aside
       id="logo-sidebar"
-      className="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-ebony-950 dark:border-gray-700"
+      className="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white sm:translate-x-0 dark:bg-ebony-950"
       aria-label="Sidebar"
     >
       <div className="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-ebony-950">
